38fd357450530d3d98e75de2dab070a5c17d96db
Sparsh Sharma <142775479+itsparsh10@users.noreply.github.com>
2024-02-21 23:47:12 +0530

    Add files via upload

---
+++ 29-Jan/Q1.cpp
@@ -0,0 +1,11 @@
+// we have two arrays first of size 28 named girls and second array named boys of size 28 and both 
+// arrays are in sorted way containing marks, we have to create a third array of size=array size of girl+ 
+// array size of boys and have to insert  the marks in sorted way
+#include<iostream>
+using namespace std;
+void merg()
+
+int main()
+{
+   return 0;
+}
